921c20614d09e8e4dfdb30c78dcc83e7792d63c3
Urdatorn <urdatorn@hotmail.com>
2025-02-17 13:25:30 +0700

    rm pycache

--- class_dummy.py
+++
@@ -1,5 +0,0 @@
-from class_syllable import Syllable
-from class_word import Word
-
-text = '''[ἀ]{έ}{να}[οι] {Νε}{φέ}[λαι]'''
-syllables = [Syllable(s) for s in text.split()]
--- plot.py
+++ plot.py
@@ -12,10 +12,10 @@ ax.plot(x, values, 'b--', linewidth=1)  # Dotted line connecting points
 
 # Formatting
 ax.set_ylabel('Accentresponsion', fontsize=12)
-ax.set_ylim(0, 50)  # Limit y-axis to 30%
+ax.set_ylim(0, 50)  # y-axis limited to 50%
 ax.set_xticks(x)
 ax.set_xticklabels(labels, fontstyle='italic', fontsize=12)
-ax.set_yticks(range(0, 41, 5))  # Ticks every 5%
+ax.set_yticks(range(0, 41, 5))  # = axis between 0 and 41%, ticks every 5%
 ax.set_title('Accentresponsion per pjäs av Aristofanes', fontsize=14)
 
 plt.savefig('plot.png', dpi=500)
